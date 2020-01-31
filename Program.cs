84be36060718713274bdab44d4b1cfbceb2935d5
codernr <zoltan.csore@protonmail.com>
2020-01-31 13:34:22 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bloggen.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

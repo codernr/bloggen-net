a884892f3b121e5d5f8942c66a0eb58dcc37be66
codernr <zoltan.csore@protonmail.com>
2020-02-03 20:01:36 +0100

    Close #1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 using System;
+using CommandLine;
 
 namespace Bloggen.Net
 {
     class Program
     {
+        public class CommandLineOptions
+        {
+            [Option('s', "source", Required = true, HelpText = "Source directory")]
+            public string SourceDirectory { get; set; }
+
+            [Option('o', "output", Required = true, HelpText = "Output directory")]
+            public string OutputDirectory { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed<CommandLineOptions>(o =>
+                {
+                    Console.WriteLine("Hello World!");
+                })
+                .WithNotParsed(errors =>
+                {
+                    Environment.Exit(1);
+                });
         }
     }
 }

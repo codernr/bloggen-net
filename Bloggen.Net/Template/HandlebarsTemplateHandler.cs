8ab21d4d995638c267151db339ecac2701dbc6f0
codernr <zoltan.csore@protonmail.com>
2020-02-05 16:48:36 +0100

    #5 Register partials

--- Bloggen.Net/Template/HandlebarsTemplateHandler.cs
+++ Bloggen.Net/Template/HandlebarsTemplateHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Bloggen.Net.Source;
 using HandlebarsDotNet;
@@ -7,16 +8,27 @@ namespace Bloggen.Net.Template
 {
     public class HandlebarsTemplateHandler : ITemplateHandler
     {
-        private readonly ISourceHandler sourceHandler;
         private readonly Action<TextWriter, object> renderLayout;
 
         public HandlebarsTemplateHandler(ISourceHandler sourceHandler)
         {
-            this.sourceHandler = sourceHandler;
-
-            using var sr = new StreamReader(this.sourceHandler.GetLayout());
+            using var sr = new StreamReader(sourceHandler.GetLayout());
 
             this.renderLayout = Handlebars.Compile(sr);
+
+            RegisterPartials(sourceHandler.GetPartials());
+        }
+
+        private static void RegisterPartials(IEnumerable<(string templateName, Stream stream)> partials)
+        {
+           foreach (var partial in partials)
+           {
+               using var sr = new StreamReader(partial.stream);
+
+               var compiled = Handlebars.Compile(sr);
+
+               Handlebars.RegisterTemplate(partial.templateName, compiled);
+           }
         }
     }
 }

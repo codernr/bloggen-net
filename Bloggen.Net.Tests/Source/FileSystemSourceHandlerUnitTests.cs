d4606b51f134499ce3f8c2dc6d590b60b7e923d8
codernr <zoltan.csore@protonmail.com>
2020-02-05 16:08:51 +0100

    #4 Add tests for FileSystemSourceHandler

---
+++ Bloggen.Net.Tests/Source/FileSystemSourceHandlerUnitTests.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using Bloggen.Net.Source;
+using Xunit;
+
+namespace Bloggen.Net.Tests
+{
+    public class FileSystemSourceHandlerUnitTests
+    {
+        [Fact]
+        public void ShouldReturnLayoutStreamWhenExists()
+        {
+            var service = this.Construct(new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { @"c:\source\layout.hbs", new MockFileData("hello") }
+            }));
+
+            Assert.IsAssignableFrom<Stream>(service.GetLayout());
+        }
+
+        [Fact]
+        public void ShouldThrowWhenLayoutDoesntExist()
+        {
+            var service = this.Construct(new MockFileSystem());
+
+            Assert.ThrowsAny<IOException>(() => service.GetLayout());
+        }
+
+        [Fact]
+        public void ShouldReturnPartialStreams()
+        {
+            var service = this.Construct(new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { @"c:\source\partials\a.hbs", new MockFileData("a") },
+                { @"c:\source\partials\b.hbs", new MockFileData("b") }
+            }));
+
+            var partials = service.GetPartials();
+            
+            Assert.Equal(2, partials.Count());
+        }
+
+        [Fact]
+        public void ShouldThrowWhenPartialsDirNotPresent()
+        {
+            var service = this.Construct(new MockFileSystem());
+
+            Assert.Throws<DirectoryNotFoundException>(() => service.GetPartials());
+        }
+
+        private FileSystemSourceHandler Construct(MockFileSystem mock)
+        {
+            return new FileSystemSourceHandler(mock, new CommandLineOptions
+            {
+                SourceDirectory = @"c:\source", OutputDirectory = @"c:\output"    
+            });
+        }
+    }
+
+}

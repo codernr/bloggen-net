e38720eeb0b0a98bfabf44f1051ff79d3e56d299
codernr <zoltan.csore@protonmail.com>
2020-02-06 18:48:05 +0100

    #9 Extend model classes with references

--- Bloggen.Net/Models/Post.cs
+++ Bloggen.Net/Models/Post.cs
@@ -4,10 +4,14 @@ namespace Bloggen.Net.Models
 {
     public class Post
     {
+        public string FileName { get; set; } = null!;
+        
         public string? Title { get; set; }
 
         public string? Excerpt { get; set; }
 
         public List<string>? Tags { get; set; }
+
+        public List<Tag> TagReferences { get; set; } = new List<Tag>();
     }
 }
--- Bloggen.Net/Models/Tag.cs
+++ Bloggen.Net/Models/Tag.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+
 namespace Bloggen.Net.Models
 {
     public class Tag
     {
         public string? Name { get; set; }
+
+        public List<Post> PostReferences { get; set; } = new List<Post>();
     }
 }

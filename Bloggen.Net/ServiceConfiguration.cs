7c562f5267486a789cde06bdb1c511e1fefb0806
codernr <zoltan.csore@protonmail.com>
2020-02-06 22:19:55 +0100

    #9 Configure services

--- Bloggen.Net/ServiceConfiguration.cs
+++ Bloggen.Net/ServiceConfiguration.cs
@@ -1,11 +1,14 @@
 using System;
 using System.IO.Abstractions;
 using Bloggen.Net.Config;
+using Bloggen.Net.Model;
 using Bloggen.Net.Source;
 using Bloggen.Net.Template;
 using HandlebarsDotNet;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace Bloggen.Net
 {
@@ -20,6 +23,10 @@ namespace Bloggen.Net
                 .Configure<SiteConfig>(siteConfiguration)
                 .AddSingleton<IFileSystem, FileSystem>()
                 .AddSingleton<ISourceHandler, FileSystemSourceHandler>()
+                .AddSingleton<IDeserializer>(new DeserializerBuilder()
+                    .WithNamingConvention(new CamelCaseNamingConvention())
+                    .Build())
+                .AddSingleton<IContext, Context>()
                 .AddSingleton<IHandlebars>(Handlebars.Create())
                 .AddSingleton<ITemplateHandler, HandlebarsTemplateHandler>();
 

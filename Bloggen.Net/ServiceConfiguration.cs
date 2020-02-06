f33e528ebb2578dbfb6d781d8ccb5e1d568b1cbc
codernr <zoltan.csore@protonmail.com>
2020-02-06 22:56:27 +0100

    #9 Update ServiceConfiguration

--- Bloggen.Net/ServiceConfiguration.cs
+++ Bloggen.Net/ServiceConfiguration.cs
@@ -1,12 +1,15 @@
 using System;
+using System.IO;
 using System.IO.Abstractions;
 using Bloggen.Net.Config;
 using Bloggen.Net.Model;
+using Bloggen.Net.Serialization;
 using Bloggen.Net.Source;
 using Bloggen.Net.Template;
 using HandlebarsDotNet;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using YamlDotNet.Core;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -24,8 +27,10 @@ namespace Bloggen.Net
                 .AddSingleton<IFileSystem, FileSystem>()
                 .AddSingleton<ISourceHandler, FileSystemSourceHandler>()
                 .AddSingleton<IDeserializer>(new DeserializerBuilder()
-                    .WithNamingConvention(new CamelCaseNamingConvention())
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
                     .Build())
+                .AddSingleton<Func<TextReader, IParser>>(tr => new Parser(tr))
+                .AddSingleton<IFrontMatterDeserializer, FrontMatterDeserializer>()
                 .AddSingleton<IContext, Context>()
                 .AddSingleton<IHandlebars>(Handlebars.Create())
                 .AddSingleton<ITemplateHandler, HandlebarsTemplateHandler>();

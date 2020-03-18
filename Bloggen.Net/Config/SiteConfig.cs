8e6457ffc3a8cd87559afc1178a861de6c7acbf8
codernr <zoltan.csore@protonmail.com>
2020-03-18 21:45:51 +0100

    Add config item

--- Bloggen.Net/Config/SiteConfig.cs
+++ Bloggen.Net/Config/SiteConfig.cs
@@ -4,6 +4,8 @@ namespace Bloggen.Net.Config
     {
         public string Title { get; set; } = string.Empty;
 
+        public string SubHeading { get; set; } = string.Empty;
+
         public string Template { get; set; } = string.Empty;
 
         public int PostsPerPage { get; set; } = 10;
